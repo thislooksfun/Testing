853014b1cd323cc242a30eac734d9e9d9cd4137d
thislooksfun <thislooksfun@repbot.org>
2017-12-10 20:43:35 -0600

    Here we go!

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
+set -ev
+
 echo "Running tests!"
+
+if [ TRAVIS_PULL_REQUEST == false ]; then
+  curl \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -d '{"title":"It'\''s working!","head":"'"$TRAVIS_REPO_SLUG:$TRAVIS_BRANCH"'","base":"master"}' \
+    "https://github.com/repos/$TRAVIS_REPO_SLUG/pulls"
+else
+  echo "Build is a pull request (#$TRAVIS_PULL_REQUEST) -- skipping!"
+fi
