edff852100914bf0946d67df2ab188d7e2c5c4c2
thislooksfun <thislooksfun@repbot.org>
2017-12-10 21:27:07 -0600

    Trying something

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,7 @@ set -e
 echo "Running tests!"
 
 if [ $TRAVIS_EVENT_TYPE == "push" ]; then
-  DATA="{\"title\":\"It's working!\",\"head\":\"$TRAVIS_BRANCH\",\"base\":\"master\"}"
+  DATA="{\"title\":\"It's working!\",\"head\":\"thislooksfun:$TRAVIS_BRANCH\",\"base\":\"master\"}"
   URL="https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls"
   
   echo "Creating PR with data:"
