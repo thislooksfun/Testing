187bf9171c67a802e05b1ad7ccdcac8e01ecc468
thislooksfun <thislooksfun@repbot.org>
2017-12-10 20:53:45 -0600

    Actually use API

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,15 +5,14 @@ set -e
 echo "Running tests!"
 
 if [ $TRAVIS_PULL_REQUEST == false ]; then
-  echo "Creating PR with data:"
-  echo "> "'{"title":"It'\''s working!","head":"'"$TRAVIS_REPO_SLUG:$TRAVIS_BRANCH"'","base":"master"}' \
-  echo "> https://github.com/repos/$TRAVIS_REPO_SLUG/pulls"
+  DATA="{\"title\":\"It's working!\",\"head\":\"$TRAVIS_REPO_SLUG:$TRAVIS_BRANCH\",\"base\":\"master\"}"
+  URL="https://api.github.com/repos/$TRAVIS_REPO_SLUG/pulls"
   
-  curl \
-    -H "Content-Type: application/json" \
-    -X POST \
-    -d '{"title":"It'\''s working!","head":"'"$TRAVIS_REPO_SLUG:$TRAVIS_BRANCH"'","base":"master"}' \
-    "https://github.com/repos/$TRAVIS_REPO_SLUG/pulls"
+  echo "Creating PR with data:"
+  echo "> $DATA"
+  echo "> $URL"
+  
+  curl -H "Content-Type: application/json" -X POST -d "$DATA" "$URL"
 else
   echo "Build is a pull request (#$TRAVIS_PULL_REQUEST) -- skipping!"
 fi
