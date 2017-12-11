0bdb933bae7f008278caf45de54f96bdde35c623
thislooksfun <thislooksfun@repbot.org>
2017-12-10 20:48:52 -0600

    Really self?

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,11 +4,10 @@ set -e
 
 echo "Running tests!"
 
-if [ TRAVIS_PULL_REQUEST == "false" ]; then
+if [ $TRAVIS_PULL_REQUEST == false ]; then
   echo "Creating PR with data:"
   echo "> "'{"title":"It'\''s working!","head":"'"$TRAVIS_REPO_SLUG:$TRAVIS_BRANCH"'","base":"master"}' \
   echo "> https://github.com/repos/$TRAVIS_REPO_SLUG/pulls"
-  
   
   curl \
     -H "Content-Type: application/json" \
